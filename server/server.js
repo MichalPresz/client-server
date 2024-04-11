569c285eaf8813338d6aa9b4ca3be5339009ac1b
MichalPresz <m.presz@wp.pl>
2024-04-11 10:14:13 +0100

    all files

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+// your routes will go here...
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000...");
+});
+app.get("/messages", (req, res) => {
+    res.json({ message: "Hello, World!" });
+  });
